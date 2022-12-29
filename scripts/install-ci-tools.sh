8c24bdd5899da60197c036478912ba5381fb6317
hadrianpaulomanu.lim <hadrianpaulomanu.lim@rakuten.com>
2022-12-29 13:31:28 +0900

    ci: setup CI and pre-commit

---
+++ scripts/install-ci-tools.sh
@@ -0,0 +1,2 @@
+cargo install cargo-tarpaulin cargo-audit
+rustup component add rustfmt clippy
---
+++ scripts/install-dev-tools-macos.sh
@@ -0,0 +1,6 @@
+brew install michaeleisel/zld/zld npm
+cargo install cargo-watch cargo-audit cargo-tarpaulin
+rustup component add rustfmt clippy
+pip install Commitizen pre-commit
+pre-commit install
+npm install -g @commitlint/cli @commitlint/config-conventional
