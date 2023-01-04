1bdb34cc2510c0aebba863249233d14995c24788
hadrianpaulomanu.lim <hadrianpaulomanu.lim@rakuten.com>
2023-01-04 16:31:54 +0900

    ci: add skip docker initialization flag

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -22,16 +22,20 @@ DB_PORT="${DB_PORT:=5432}"
 
 DATABASE_URL="postgres://${DB_USER}:${DB_PASSWORD}@localhost:${DB_PORT}/${DB_NAME}"
 
-docker run \
-    -e POSTGRES_USER="${DB_USER}" \
-    -e POSTGRES_PASSWORD="${DB_PASSWORD}" \
-    -e POSTGRES_DB="${DB_NAME}" \
-    -p "${DB_PORT}":5432 \
-    -d postgres \
-    postgres -N 1000
+if [[ -z "${SKIP_DOCKER}" ]]
+then
+    # Start the database
+    docker run \
+        -e POSTGRES_USER="${DB_USER}" \
+        -e POSTGRES_PASSWORD="${DB_PASSWORD}" \
+        -e POSTGRES_DB="${DB_NAME}" \
+        -p "${DB_PORT}":5432 \
+        -d postgres \
+        postgres -N 1000
+fi
 
 export PGPASSWORD="${DB_PASSWORD}"
-until psql -h localhost -U "${DB_USER}" -p "${DB_PORT}" -lqt | cut -d \| -f 1 | grep -qw "${DB_NAME}"; do
+until psql -h localhost -U "${DB_USER}" -p "${DB_PORT}" -d "postgres" -c '\q'; do
     >&2 echo "Postgres is unavailable - sleeping"
     sleep 1
 done
